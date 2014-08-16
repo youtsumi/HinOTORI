0659539073729a8fdfd454be1f63c41a6d91d9f8
Yousuke Utsumi <youtsumi@hiroshima-u.ac.jp>
2014-08-16 19:36:37 +0900

    add TelescopeClient.py

---
+++ TelescopeClient.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys, traceback, Ice, os
+Ice.loadSlice("HinOTORI.ice")
+import HinOTORI
+from optparse import OptionParser
+
+status = 0
+
+class CameraClient(Ice.Application):
+	def run(self,args):
+		self.args=args
+		self.shutdownOnInterrupt()
+		self.TelescopeProcessor()
+
+	def TelescopeProcessor(self):
+		"""
+		A class method to communicate with the telescope.
+		"""
+		obj = self.communicator().stringToProxy("telescope:default -h 192.168.0.40 -p 10001")
+		telescope=HinOTORI.TelescopePrx.checkedCast(obj)
+
+		if self.args[1] == "open":
+			print "open"
+			telescope.OpenMirror()
+		elif self.args[1] == "close":
+			print "close"
+			telescope.CloseMirror()
+		else:
+			print "no such command %s" % self.args[1]
+
+
+if __name__ == "__main__":
+	app = CameraClient()
+	status = app.main(sys.argv)
+	sys.exit(status)
+
