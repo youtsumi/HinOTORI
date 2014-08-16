e18a0824ee6ef3d6264459eaea740422e78279c7
Yousuke Utsumi <youtsumi@hiroshima-u.ac.jp>
2014-08-16 14:26:10 +0900

    update CameraClient.py to do test MountSrv

--- CameraClient.py
+++ CameraClient.py
@@ -4,7 +4,6 @@ Ice.loadSlice("HinOTORI.ice")
 import HinOTORI
 from optparse import OptionParser
 
-
 status = 0
 camnum = 3
 
@@ -13,17 +12,33 @@ class CameraClient(Ice.Application):
 		self.shutdownOnInterrupt()
 		self.parseoptions(args)
 		self.TelescopeProcessor()
+		self.MountProcessor()
 		self.CameraProcessor()
 
 	def TelescopeProcessor(self):
-		obj = self.communicator().stringToProxy("telescope:default -p 10001")
+		"""
+		A class method to communicate with the telescope.
+		"""
+		obj = self.communicator().stringToProxy("telescope:default -h 192.168.0.40 -p 10001")
 		telescope=HinOTORI.TelescopePrx.checkedCast(obj)
 		if self.options.focusz!=None:
-			telescope.SetFocusZ(float(self.options.focusz))
+			telescope.SetFocusZ(float(self.options.focusz)*10000)
 		self.z=telescope.GetFocusZ()
 		print "Focus z = %lf [mm]" % self.z
 
+	def MountProcessor(self):
+		"""
+		A class method to communicate with the mount.
+		"""
+		obj = self.communicator().stringToProxy("Mount:default -p 10002")
+		mount=HinOTORI.MountPrx.checkedCast(obj)
+		print mount.GetRa(), mount.GetDec()
+
 	def CameraProcessor(self):
+		"""
+		A class method to communicate with the cameras
+		"""
+
 		cameras = []
 		aptr = []	# array for an pointer to the asynchronous method invocation
 
@@ -39,6 +54,9 @@ class CameraClient(Ice.Application):
 			cameras[i].end_Take(aptr[i])
 
 	def parseoptions(self,args):
+		"""
+		A class method to parse the argument
+		"""
 		parser = OptionParser()
 		parser.add_option("-z", "--focus-z", dest="focusz",
                   help="set focus z", metavar="FILE")
