6a1781d240dae4cc9bf5f67daa461d68bd45aca4
Yousuke Utsumi <youtsumi@hiroshima-u.ac.jp>
2014-08-19 12:25:12 +0900

    implement config file to distribute common information

---
+++ config.py
@@ -0,0 +1,32 @@
+camera=[
+	{
+		'serial': 131741,
+		'filter': 'u',
+		'uid': 0
+	},
+	{
+		'serial': 121430,
+		'filter': 'Rc',
+		'uid':	1
+	},
+	{
+		'serial': 121431,
+		'filter': 'Ic',
+		'uid': 2
+	}
+]
+
+nodesetting={
+	'camera': { 
+		'ip': "127.0.0.1",
+		'port': 10000
+		},
+	'mount' : {
+		'ip': "127.0.0.1",
+		'port': 10002
+		},
+	'telescope': {
+		'ip': "192.168.0.40",
+		'port': 10001
+		}
+}
