172833558e59e308334b5d0548364c4aeef90e8c
Yousuke Utsumi <youtsumi@gmail.com>
2015-09-10 17:14:30 +0900

    update ERDEToolkit

--- ERDEToolKit.py
+++ ERDEToolKit.py
@@ -1,170 +1,201 @@
-#coding: utf-8
-"""
-A python script to handle ERDE TRDC2 Software.
-2015.9.1 Yousuke Utsumi
-"""
-from pywinauto import application, controls
+#!/usr/bin/env python
+import serial
 import time
-import config
-import threading
-import logging
-import tesseract
+import re
+from multiprocessing import Manager, Process
 
-pathtoapp = "C:\ERDE\TRDC2.exe"
-windowname = u'ERDE.+'
+length=22
+p=re.compile(r"FB_.*")
+grammer={
+        "FB_DOME_RIGHT":            ( 3,  5,   0x8),
+        "FB_DOME_LEFT":                        ( 3,  5,   0x4),
+        "FB_DOME_STOP":                        ( 3,  5,   0x2),
+        "FB_SLIT_OPEN":                        ( 3,  5,   0x1),
+        "FB_SLIT_CLOSE":                ( 3,  6,   0x8),
+        "FB_SLIT_STOP":                        ( 3,  6,   0x4),
+        "FB_DOME_MANUAL":            ( 3,  6,   0x2),
+        "FB_SLIT_AUTO_CLOSE":            ( 3,  6,   0x1),
+        "DOME_OPENED":                    ( 3,  7,   0x8),
+        "DOME_CLOSED":                    ( 3,  7,   0x4),
+        "ALARM1":                    ( 3,  7,   0x2),
+        "ALARM2":                        ( 3,  7,   0x1),
+        "ALARM3":                        ( 3,  8,   0x8),
+        "FB_DOME_EMG_STOP":            ( 3,  8,   0x4),
+        "ORIGIN":                    ( 3,  8,   0x2),
+        "FB_LATCH":                    ( 3,  8,   0x1),
+        "LST.hh":                        ( 5,  5,  0xff),
+        "LST.mm":                        ( 5,  7,  0xff),
+        "LST.ss":                        ( 5,  9,  0xff),
+        "LST.t":                        ( 5, 10,   0xf),
+        "RA.hh":                        ( 6,  5,  0xff),
+        "RA.mm":                        ( 6,  7,  0xff),
+        "RA.ss":                        ( 6,  9,  0xff),
+        "RA.t":                                ( 6, 11,   0xf),
+        "DEC.sign":                     ( 6, 12,   0xf),
+        "DEC.hh":                        ( 6, 13,  0xff),
+        "DEC.mm":                        ( 6, 15,  0xff),
+        "DEC.ss":                        ( 6, 17,  0xff),
+        "JST.YY":                        ( 7,  5,  0xff),
+        "JST.MM":                        ( 7,  7,  0xff),
+        "JST.DD":                        ( 7,  9,  0xff),
+        "JST.hh":                        ( 7, 11,  0xff),
+        "JST.mm":                        ( 7, 13,  0xff),
+        "JST.ss":                        ( 7, 15,  0xff),
+        "TARGETDIR":                        ( 8,  5, 0xfff),
+        "CURRENTDIR":                        ( 8,  8, 0xfff),
+        "TELESCOPE":                        ( 9,  5,  0xff),
+        "LATCHTIMER":                        (10,  5, 0xfff),
+    }
 
-logging.basicConfig(format=config.FORMAT, level=config.loglevel)
-logger=logging.getLogger(__name__)
 
-class Dome:
-    def __init__(self):
-        pass
+def MakeNormalMessage( address ):
+    return ":WR%02d"%address+"F"*14+"01#"
 
-    def __del__(self):
-        self.app.kill_()
- 
-    def Connect(self):
-        """Try to connect to the TRDC software"""
-        try:
-            self.app = application.Application()
-            self.app.connect_(path=pathtoapp)
-            logger.info("TRDC is already running... Try to fetch the handle.")
-        except application.ProcessNotFoundError:
-            logger.info("Try to run TRDC application")
-            self.app = application.Application.start(pathtoapp)
+def PressButton( key ):
+    normal = MakeNormalMessage(3)
+    msg =  normal
+    register, offset, mask=grammer[key]
+    if register != 3:
+        raise Exception("Invalid key was specified")
+    msg=msg[:offset]+'%X' % ( int(msg[offset],16)^mask )+msg[offset+1:]
+    return msg
+
+def OneCommand(line):
+
+    status = {}
+    for k, v in grammer.iteritems():
+        register, offset, mask = v
+        if register!=int(line[3:5],16):
+            continue
+        if mask>=0xf:
+            value=line[offset:offset+mask.bit_length()/4]
+        else:
+            value=0 if int(line[offset],16)&mask==0 else 1
+#	print k, offset, mask, value
+        status.update({k:value})
+
+    return status
+
+
+def SocketManager( port, status, sendbuf):
+    ser=serial.Serial(port,38400,timeout=0,parity=serial.PARITY_ODD, stopbits=1)
+    msg=""
+    while True:
+	msg+=ser.read(100)
+	idx1 = msg.find(":AS03")
+	if idx1 == -1:
+	    continue
+	idx2 = msg.find(":AS03",idx1+1)
+	if idx2 == -1:
+	    continue
+	line=msg[idx1:idx2]
+
+	stx=0
+	result = {}
+	while len(line)>length:
+	    line=line[stx:]
+	    stx=line.index(":")
+	    etx=line.index("#",stx)
+	    status.update(OneCommand(line[stx:etx+1]))
+	    stx=etx+1                
+
+	# clear buffer if the queue is too long
+	if len(msg)>1024:
+	    msg = ""
+	else:
+	    msg = msg[idx2:]
+
+	# send message
+	if len(sendbuf)==0:
+	    continue
+
+	print sendbuf
+	sendmsg=sendbuf.pop(0)
+	print "Send this message: ", sendmsg
+	ser.write(sendmsg)
         
-        self.app_form = self.app.window_(title_re=windowname)
-#        print self.app.window_(title_re="RS232.+").isDialog()
-#        if self.app.window_(title_re="RS232.+"):
-#            logger.error("RS232C port is not avialable")
-#            self.app.window_(title_re="RS232.+")["OK"].Click()
-#        raise Exception("RS232C port is not avialable")
-        
-    def GetStatus(self):
-	for item in  [ u"RAStatic2",
-		 u"DECStatic2",
-		 u"Button9", # rotate auto
-		 u"Button11", # slit close
-		 u"Button12", # latch auto
-		 u"アラーム１", # alarm 1
-		 u"アラーム２", # alarm 2
-		 u"アラーム３", # alarm 3
-		 u"Static10", # target angle
-		 u"Static13" ] : # current angle
-	    print item, self.CheckState(item)
+class TRDCToolKit:
+    def __init__(self,port="COM6"):
+        manager = Manager()
+        self.status=manager.dict()
+        self.sendbuf=manager.list()
+        self.p = Process(target=SocketManager, args=("COM6",self.status,self.sendbuf) )
+	self.p.daemon=True
+        self.p.start()
 
-    def MakeAllRemote(self):
-	logger.error("Make all remote")
-	for item in [
-		 u"Button9", # rotate auto
-		 u"Button11", # slit close
-		 u"Button12", # latch auto
-		    ]:
-	    button=self.app_form[item]
-	    if button.CaptureAsImage().getcolors() == (255,0,0):
-		button.Click()
+    def __com(self,msg):
+	self.sendbuf.append(msg)
+	self.sendbuf.append(MakeNormalMessage(3))
 
-    def SlitOpen(self):
-	logger.info("Try to open the dome slit")
-	button=self.app_form[u"Button4"] # left button for what? maybe slit close
-	button.Click()
+    def __ToggleButton(self,key,state):
+        if state==True:
+            if self.status[key]==1:
+                return
+            self.__com(PressButton(key))
+        else:
+            if self.status[key]==0:
+                return
+            self.__com(PressButton(key))
 
-    def SlitClose(self):
-	logger.info("Try to close the dome slit close")
-	button=self.app_form[u"Button2"] # left button for what? maybe slit close
-	button.Click()
+    def SlitAutoCloseOff(self,state):
+        self.__ToggleButton("FB_SLIT_AUTO_CLOSE",state)
 
-    def CheckState(self,item):
-	button=self.app_form[item]
-	for i, v in button.CaptureAsImage().getcolors():
-	    if v == ( 255, 0, 0 ) :
-		return False
-	    elif v == ( 0, 255, 0 ):
-		return True
-	    else:
-		pass
-	return None
+    def SlitOpen(self,state=True):
+        if self.status["DOME_OPENED"]==1:
+            return
+        self.__ToggleButton("FB_SLIT_OPEN",state)
 
-    def InspectItem(self,item):
-	"""This method is prepared for developing purpose"""
-#    self.app_form.PrintControlIdentifiers()
-	button=self.app_form[item]
-	print item, 
-	for i, v in button.CaptureAsImage().getcolors():
-	    if v == ( 255, 0, 0 ) :
-		print "RED",
-	    elif v == ( 0, 255, 0 ):
-		print "GREEN",
-	    else:
-		pass
-	print
+    def SlitClose(self,state=True):
+        if self.status["DOME_CLOSED"]==1:
+            return
+        self.__ToggleButton("FB_SLIT_CLOSE",state)
 
-	button.CaptureAsImage().save("%s.jpg" % item)
+    def DomeManual(self,state):
+        self.__ToggleButton("FB_DOME_MANUAL",state)
 
-
-    def readcontent(self):
-	import tesseract
-	api = tesseract.TessBaseAPI()
-    #    api.SetOutputName("outputName");
-	api.Init(".","eng",tesseract.OEM_DEFAULT)
-	api.SetPageSegMode(tesseract.PSM_AUTO)
-	mImgFile = "eurotext.jpg"
-	pixImage=tesseract.pixRead(mImgFile)
-	api.SetImage(pixImage)
-	outText=api.GetUTF8Text()
-	print("OCR output:\n%s"%outText);
-	api.End()
+    def DomeLatchDisable(self,state):
+        self.__ToggleButton("FB_LATCH",state)
 
 if __name__ == "__main__":
-    import time, sys, traceback
-    try:
-	dome = Dome()
-	dome.Connect()
-	dome.GetStatus()
-	dome.MakeAllRemote()
-	dome.SlitOpen()
-	time.sleep(10)
-	dome.SlitClose()
-	time.sleep(10)
-	exit(0)
-	dome.InspectItem(u"Button1") # submit time
-	dome.InspectItem(u"Button2") # left button for what? maybe slit close
-	dome.InspectItem(u"Button3") # stop button maybe slit close
-	dome.InspectItem(u"Button4") # right button for what? maybe slit close
-	dome.InspectItem(u"Button5") # submit button same as 10
-	dome.InspectItem(u"Button6") # left button for the rotation
-	dome.InspectItem(u"Button7") # stop button for the rotation
-	dome.InspectItem(u"Button8") # right button for the rotation
-	dome.InspectItem(u"Button10") # submit 
-	dome.InspectItem(u"Static5")
-	dome.InspectItem(u"Static6") # telescope ra
-	dome.InspectItem(u"Static7")
-	dome.InspectItem(u"Static8") # telescope dec
-	dome.InspectItem(u"Static9")
-	dome.InspectItem(u"Static10")
-	dome.InspectItem(u"Static11")
-	dome.InspectItem(u"Static12")
-	dome.InspectItem(u"Static14")
-	dome.InspectItem(u"Static15")
-	dome.InspectItem(u"Static16")
-	dome.InspectItem(u"Static17")
-	dome.InspectItem(u"Static18")
-	dome.InspectItem(u"Static19")
-	dome.InspectItem(u"Static20")
-	dome.InspectItem(u"Static21")
-	dome.InspectItem(u"Static22")
-	dome.InspectItem(u"Static23")
-	dome.InspectItem(u"Static24")
-	dome.InspectItem(u"Static25")
-	dome.InspectItem(u"Static26")
-	dome.InspectItem(u"Static27") # dome origin
-	dome.InspectItem(u"Static28")
-	dome.InspectItem(u"ドーム基準")
-	dome.app_form.PrintControlIdentifiers()
-
-
-    except:
-        traceback.print_exc(file=sys.stdout)
-        pass
-#        del dome
-
+#    Dome = TRDCToolKitSimulator()
+#    SocketReceiver({})
+    Dome = TRDCToolKit()
+##    for i in range(1000):
+##        time.sleep(0.5)
+##        print "current: ", Dome.status
+    time.sleep(2)
+#    print Dome.status["FB_SLIT_AUTO_CLOSE"]
+#    Dome.SlitAutoCloseOff(False)
+#    time.sleep(2)
+#    print Dome.status["FB_SLIT_AUTO_CLOSE"]
+#    time.sleep(5)
+#    Dome.SlitAutoCloseOff(True)
+#    time.sleep(2)
+#    print Dome.status["FB_SLIT_AUTO_CLOSE"]
+#    time.sleep(5)
+#    Dome.SlitAutoCloseOff(False)
+#    print Dome.status["FB_SLIT_AUTO_CLOSE"]
+#    time.sleep(5)
+#    time.sleep(5)
+#    Dome.DomeManual(True)
+#    time.sleep(5)
+#    Dome.DomeManual(False)
+#    time.sleep(5)
+#    Dome.DomeManual(True)
+#    time.sleep(5)
+#    print Dome.status["FB_LATCH"]
+#    Dome.DomeLatchDisable(False)
+#    time.sleep(5)
+#    print Dome.status["FB_LATCH"]
+#    Dome.DomeLatchDisable(True)
+#    time.sleep(5)
+#    print Dome.status["FB_LATCH"]
+#    Dome.DomeLatchDisable(False)
+#    time.sleep(5)
+#    print Dome.status["FB_LATCH"]
+#    
+#        
+    Dome.SlitOpen()
+    time.sleep(5)
+#    Dome.SlitClose()
+#    time.sleep(1000)
