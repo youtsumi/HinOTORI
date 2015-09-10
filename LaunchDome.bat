7f1cdef0a76baa1287b5b42477fae764b2f3b379
Yousuke Utsumi <youtsumi@gmail.com>
2015-09-10 18:29:15 +0900

    add a batch file to handle Dome Server

---
+++ LaunchDome.bat
@@ -0,0 +1,18 @@
+?@echo off
+rem
+rem A batch file to support AllunaToolKit
+rem
+
+rem Settings
+set PATH=%PATH%;"c:\Python27"
+set PYTHONPATH=%PYTHONPATH%;"C:\Program Files\ZeroC\Ice-3.5.1\python"
+set today_YYYYMMDD=%date:~0,4%%date:~5,2%%date:~8,2%
+
+
+rem Change the directory to the current directory.
+pushd %0\..
+cls
+
+python DomeSrv.py >> %0\..\Dome%today_YYYYMMDD%.log 2>&1 &
+
+exit
